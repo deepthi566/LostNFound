bf31dcc2ed83406fd80e37f93db89acc8ecd1742
Ramya Deepthi Jalli <deepthij0607@gmail.com>
2026-01-17 16:42:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ const app = express();
 app.use("/uploads", express.static("uploads"));
 app.use(cors());
 app.use(express.json()); // MIDDLEWAREparses incoming JSON request bodies and fills req.body.()
+app.get("/", (req, res) => {
+  res.send("Lost and Found backend is running!");
+});
 const port = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
